a72684cafd60aa7ceebb5b193062355524f9f276
Heynon <Heynon@163.com>
2019-08-16 17:41:11 +0800

    修改

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,6 @@ package test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello,world!");
-		System.out.println("第一次使用");
+		System.out.println("第一次使用,就是好");
 	}
 }
